1233b6c8e5a9077920be0c1c3b60b7a7836d4d04
Rohith Patel <rohith.patel@pearson.com>
2023-10-18 12:15:59 -0700

    refactor to mvc

---
+++ live-vanilla-build/js/view.js
@@ -0,0 +1,126 @@
+export default class View {
+  $ = {};
+  $$ = {};
+
+  constructor() {
+    this.$.menu = this.#qs("[data-id ='menu']");
+    this.$.menuBtn = this.#qs("[data-id = 'menu-btn");
+    this.$.menuItems = this.#qs("[data-id ='menu-items']");
+    this.$.resetBtn = this.#qs("[data-id ='reset-btn']");
+    this.$.newRoundBtn = this.#qs("[data-id ='new-round-btn']");
+    this.$.modal = this.#qs("[data-id ='modal']");
+    this.$.modalText = this.#qs("[data-id ='modal-text']");
+    this.$.modalBtn = this.#qs("[data-id ='modal-btn']");
+    this.$.turn = this.#qs("[data-id ='turn']");
+    this.$.p1wins = this.#qs("[data-id='p1-wins']");
+    this.$.p2wins = this.#qs("[data-id='p2-wins']");
+    this.$.ties = this.#qs("[data-id = 'ties']");
+
+    this.$$.squares = this.#qsAll("[data-id ='square']");
+
+    //UI-only event listeners
+    this.$.menuBtn.addEventListener("click", (event) => {
+      this.#toggleMenu();
+    });
+  }
+
+  // Register all the event listeners
+
+  bindGameResetEvent(handler) {
+    this.$.resetBtn.addEventListener("click", handler);
+    this.$.modalBtn.addEventListener("click", handler);
+  }
+
+  bindNewRoundEvent(handler) {
+    this.$.newRoundBtn.addEventListener("click", handler);
+  }
+
+  bindPlayerMoveEvent(handler) {
+    this.$$.squares.forEach((square) => {
+      square.addEventListener("click", () => handler(square));
+    });
+  }
+
+  handlePlayerMove(squareEl, player) {
+    const icon = document.createElement("i");
+    icon.classList.add("fa-solid", player.iconClass, player.colorClass);
+    squareEl.replaceChildren(icon);
+  }
+  setTurnIndicator(player) {
+    const icon = document.createElement("i");
+    const label = document.createElement("p");
+
+    icon.classList.add("fa-solid", player.colorClass, player.iconClass);
+
+    label.classList.add(player.colorClass);
+
+    label.innerText = `${player.name}, you're up!`;
+
+    this.$.turn.replaceChildren(icon, label);
+  }
+
+  // DOM Helper methods
+  updateScoreBoard(p1Wins, p2Wins, ties) {
+    this.$.p1wins.innerText = `${p1Wins} wins`;
+    this.$.p2wins.innerText = `${p2Wins} wins`;
+    this.$.ties.innerText = `${ties}`;
+  }
+
+  openModal(message) {
+    this.$.modal.classList.remove("hidden");
+    this.$.modalText.innerText = message;
+  }
+
+  closeAll() {
+    this.#closeModal();
+    this.#closeMenu();
+  }
+
+  clearMoves() {
+    this.$$.squares.forEach((square) => {
+      square.replaceChildren();
+    });
+  }
+
+  #closeModal() {
+    this.$.modal.classList.add("hidden");
+  }
+
+  #closeMenu() {
+    this.$.menuItems.classList.add("hidden");
+    this.$.menuBtn.classList.remove("border");
+
+    const icon = this.$.menuBtn.querySelector("i");
+
+    icon.classList.add("fa-chevron-down");
+    icon.classList.remove("fa-chevron-up");
+  }
+
+  #toggleMenu() {
+    this.$.menuItems.classList.toggle("hidden");
+    this.$.menuBtn.classList.toggle("border");
+
+    const icon = this.$.menuBtn.querySelector("i");
+
+    icon.classList.toggle("fa-chevron-down");
+    icon.classList.toggle("fa-chevron-up");
+  }
+
+  #qs(selector, parent) {
+    const el = parent
+      ? parent.querySelector(selector)
+      : document.querySelector(selector);
+
+    if (!el) throw new Error("Could not find elements");
+
+    return el;
+  }
+
+  #qsAll(selector) {
+    const elList = document.querySelectorAll(selector);
+
+    if (!elList) throw new Error("Could not find elements");
+
+    return elList;
+  }
+}
